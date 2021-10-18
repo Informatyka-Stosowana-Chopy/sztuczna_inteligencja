3c4d8df4b049f72121d457f8e866ae962536a11b
Przemyslaw Lis <229940@edu.p.lodz.pl>
2021-10-18 14:13:07 +0200

    initial commit

---
+++ src/algorythms.py
@@ -0,0 +1,24 @@
+class Algorithm:
+    def __init__(self):
+        pass
+
+    def bfs(self):
+        """
+        wszerz
+        :return:
+        """
+        pass
+
+    def dfs(self):
+        """
+        wgłąb
+        :return:
+        """
+        pass
+
+    def a_star(self):
+        """
+        heurystyka
+        :return:
+        """
+        pass
---
+++ src/fifteen_puzzle.py
@@ -0,0 +1,3 @@
+class FifteenPuzzle:
+    def __init__(self):
+        pass

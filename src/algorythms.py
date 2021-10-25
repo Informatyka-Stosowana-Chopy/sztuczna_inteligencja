9df52c54cc29b4e312bf66fa04d426640d12ff4e
Przemyslaw Lis <229940@edu.p.lodz.pl>
2021-10-25 09:41:50 +0200

    add is_valid_move algorythm

--- src/algorythms.py
+++ src/algorythms.py
@@ -1,10 +1,16 @@
 class Algorithm:
-    def __init__(self):
-        self.current_bord = []
+    def __init__(self, board: list):
+        self.current_bord = board
         self.children = []
         self.valid_moves = []
+        self.MOVES = 'LRUP'
+        self.SOLVED_BOARD = [[1, 2, 3, 4],
+                             [5, 6, 7, 8],
+                             [9, 10, 11, 12],
+                             [13, 14, 15, 0]]
+        self.move_counter = 0
 
-    def bfs(self, board: list):
+    def bfs(self):
         """
         wszerz
 
@@ -13,7 +19,7 @@ class Algorithm:
         check if the bord isn't in closed list
         check valid moves
         get children
-        check if the childres are solved
+        check if the children are solved
         append open list with children (if already doesn't exist)
         if it is ont valid board, then remove from open list and add to closed
         :return:
@@ -22,24 +28,64 @@ class Algorithm:
         open_list = set()
         closed_list = set()
 
-    def is_valid_move(self, direction: str):
+        if self.current_bord == self.SOLVED_BOARD:
+            # TODO
+            return self.move_counter
+
+        if self.current_bord not in closed_list:
+            self.is_valid_move()
+            self.get_children()
+
+    def is_valid_move(self):
         """
         check if given direction is valid,
         f.g. if there is direction = 'L' it have to check if 0 is not on the extreme left
-        :param direction:
         :return:
         """
-        pass
-        # self.valid_moves - clear
-        # append self.valid_moves with valid move
+        flag = True
+        self.valid_moves = []
+
+        for move in self.MOVES:
+            if move == 'L':
+                for i in range(4):
+                    if 0 in self.current_bord[i][0]:
+                        flag = False
+                        break
+                if flag:
+                    self.valid_moves.append('L')
+            elif move == 'R':
+                for i in range(4):
+                    if 0 in self.current_bord[i][3]:
+                        flag = False
+                        break
+                if flag:
+                    self.valid_moves.append('R')
+            elif move == 'U':
+                for i in range(4):
+                    if 0 in self.current_bord[0][i]:
+                        flag = False
+                        break
+                if flag:
+                    self.valid_moves.append('U')
+            elif move == 'D':
+                for i in range(4):
+                    if 0 in self.current_bord[0][3]:
+                        flag = False
+                        break
+                if flag:
+                    self.valid_moves.append('D')
+            else:
+                raise "Not valid move"
 
     def get_children(self):
         """
         inside if statement append self.children with children of current board
         :return:
         """
+        self.children = []
+
         if 'L' in self.valid_moves:
-            pass
+            self.children.append([])  # TODO append with current board with moved 0
         elif 'R' in self.valid_moves:
             pass
         elif 'U' in self.valid_moves:

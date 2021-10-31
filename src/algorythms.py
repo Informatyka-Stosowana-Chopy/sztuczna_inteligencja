4fe79d9e4f3a6e9b10b5694c9c289df43a916bec
Przemyslaw Lis <229940@edu.p.lodz.pl>
2021-10-31 12:33:13 +0100

    correct spell mistake, add print values, working project

--- src/algorythms.py
+++ src/algorythms.py
@@ -1,9 +1,15 @@
 from copy import deepcopy
 
+# TODO
+#   make board as 2D numpy array
+#   they may be hashable
+#   TODO check if they are
+#       then if yes change open_list and closed_list to set type
+
 
 class Algorithm:
     def __init__(self, board: list):
-        self.current_bord = board
+        self.current_board = board
         self.children = []
         self.valid_moves = []
         self.MOVES = 'LRUD'
@@ -32,19 +38,21 @@ class Algorithm:
         open_list = []
         closed_list = []
         # TODO do sth to change list to set
-        while self.current_bord != self.SOLVED_BOARD:
-            if self.current_bord not in closed_list:
+
+        open_list.append(self.current_board)
+        while self.current_board != self.SOLVED_BOARD:
+            if self.current_board not in closed_list:
                 self.get_children()
-                open_list.append(self.children)
-                closed_list.append(self.current_bord)
+                for child in self.children:
+                    open_list.append(child)
+                closed_list.append(self.current_board)
 
-            self.current_bord = deepcopy(open_list[self.move_counter])
             open_list[self.move_counter] = None
+            self.print_all_values()
             self.move_counter += 1
+            self.current_board = open_list[self.move_counter]
 
-
-
-        return "solved"  # TODO
+        return f"solved in {self.move_counter - 1} moves"  # TODO
 
     def _is_valid_move(self):
         """
@@ -56,21 +64,20 @@ class Algorithm:
         self.valid_moves = ['L', 'R', 'U', 'D']
 
         for i in range(4):
-            if self.current_bord[i][0] == 0:
+            if self.current_board[i][0] == 0:
                 self.valid_moves.remove('L')
-            if self.current_bord[i][3] == 0:
+            if self.current_board[i][3] == 0:
                 self.valid_moves.remove('R')
-            if self.current_bord[0][i] == 0:
+            if self.current_board[0][i] == 0:
                 self.valid_moves.remove('U')
-            if self.current_bord[3][i] == 0:
+            if self.current_board[3][i] == 0:
                 self.valid_moves.remove('D')
 
     def __search_zero(self):
-        for i in range(4):
-            for j in range(4):
-                if self.current_bord[i][j] == 0:
-                    return i, j
-        return ValueError
+        for y in range(4):
+            for x in range(4):
+                if self.current_board[y][x] == 0:
+                    return y, x
 
     def get_children(self):
         """
@@ -82,11 +89,11 @@ class Algorithm:
         y, x = self.__search_zero()
 
         if 'L' in self.valid_moves:
-            left_board = deepcopy(self.current_bord)
+            left_board = deepcopy(self.current_board)
             left_board[y][x], left_board[y][x - 1] = left_board[y][x - 1], left_board[y][x]
             self.children.append(left_board)
         if 'R' in self.valid_moves:
-            right_board = deepcopy(self.current_bord)
+            right_board = deepcopy(self.current_board)
             right_board[y][x], right_board[y][x + 1] = right_board[y][x + 1], right_board[y][x]
             self.children.append(right_board)
         if 'U' in self.valid_moves:
@@ -94,18 +101,31 @@ class Algorithm:
             to go up we have to decrease value because it isn't cartesian system.
             it starts from top left corner and this is (0, 0) point.
             """
-            up_board = deepcopy(self.current_bord)
+            up_board = deepcopy(self.current_board)
             up_board[y][x], up_board[y - 1][x] = up_board[y - 1][x], up_board[y][x]
             self.children.append(up_board)
         if 'D' in self.valid_moves:
             """
             according to above, there is + to go down.
             """
-            down_board = deepcopy(self.current_bord)
+            down_board = deepcopy(self.current_board)
             down_board[y][x], down_board[y + 1][x] = down_board[y + 1][x], down_board[y][x]
             self.children.append(down_board)
 
-
+    def print_all_values(self):
+        print(f"TURN: {self.move_counter}")
+        print(self.current_board)
+        print(self.SOLVED_BOARD)
+        print("\n")
+        print("CURRENT BOARD:")
+        for line in self.current_board:
+            print(line)
+        print("\n")
+        print("CHILDREN:")
+        for child in self.children:
+            print(child)
+        print("\n")
+        print(f"Valid moves =  {self.valid_moves}")
 
 
 #####################################3
--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from algorythms import Algorithm
 
 board = [[1, 2, 3, 4],
          [5, 6, 7, 8],
-         [9, 10, 0, 12],
-         [13, 14, 15, 11]]
+         [9, 10, 0, 11],
+         [13, 14, 15, 12]]
 
 bfs = Algorithm(board)
 print(bfs.bfs())

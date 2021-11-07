e70cd1915ce7bebce65adea52194e56e1b143667
Przemyslaw Lis <229940@edu.p.lodz.pl>
2021-11-07 16:58:18 +0100

    initialize all tests

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+class Test:
+    def test_reader(self):
+        """this test verify if board is read properly"""
+        pass
+
+    def test_is_valid_moves(self):
+        """this test verify if valid moves are properly returned"""
+        pass
+
+    def test_search_zero(self):
+        """this test verify if zero is searched"""
+        pass
+
+    def test_get_children(self):
+        """this test verify if children are proper and if the list is in proper length"""
+        pass
+
+    def test_change_list_to_tuple(self):
+        """this test verify if list is changed to tuple"""
+        pass
+
+    def test_update_list(self):
+        """this test verify if tuple is changed to list"""
+        pass

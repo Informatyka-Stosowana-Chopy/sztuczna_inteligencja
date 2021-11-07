872d45f0c8932a91ea60f76f5279814df1fc4812
Przemyslaw Lis <229940@edu.p.lodz.pl>
2021-11-07 16:36:41 +0100

    biutify code

--- src/main.py
+++ src/main.py
@@ -1,11 +1,41 @@
 from bfs import Bfs
+from dfs import Dfs
 from reader import Reader
 import time
 
+#####################################
+# CONDITION
+#####################################
+
 board = Reader.read()
+
+#####################################
+# DFS
+#####################################
+dfs = Dfs(board)
+
+begin_dfs = time.time()
+dfs.simulation()
+end_dfs = time.time()
+
+#####################################
+# BFS
+#####################################
 bfs = Bfs(board)
-begin = time.time()
-print(bfs.simulation())
+
+begin_bfs = time.time()
+bfs.simulation()
+end_bfs = time.time()
+
+#####################################
+# PRINT RESULTS
+#####################################
+print("\n")
+print("#################################")
+print(f"DFS time: {end_dfs - begin_dfs}")
+print(f"TURNS: {dfs.move_counter}")
+print("#################################")
+print(f"BFS time: {end_bfs - begin_bfs}")
 print(f"TURNS: {bfs.move_counter}")
-end = time.time()
-print(f"Time: {end - begin}")
+print("#################################")
+print("\n")

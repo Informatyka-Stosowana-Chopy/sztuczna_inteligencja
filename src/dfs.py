1516342928c9aef5643643434cedc35682875275
Przemyslaw Lis <229940@edu.p.lodz.pl>
2021-11-07 16:36:23 +0100

    implement dfs as LIFO

--- src/dfs.py
+++ src/dfs.py
@@ -1,4 +1,5 @@
 from algorythms import Algorithm
+from queue import LifoQueue
 
 
 class Dfs(Algorithm):
@@ -7,21 +8,21 @@ class Dfs(Algorithm):
         super().__init__(board)
 
     def simulation(self):
-        open_list = set()
+        open_list = LifoQueue()
         closed_list = set()
-        # TODO do sth to change list to set
 
-        open_list.add(self.current_board_tuple)
+        open_list.put(self.current_board_tuple)
         while self.current_board != self.SOLVED_BOARD:
-            self.current_board_tuple = open_list.pop()
+            self.current_board_tuple = open_list.get()
             self.update_list()
             if self.current_board == self.SOLVED_BOARD:
                 return f"solved in {self.move_counter - 1} moves"  # TODO
 
             if self.current_board_tuple not in closed_list:
                 self.get_children()
-                for child in self.children:
-                    open_list.add(child)
+                for child in reversed(self.children):
+                    if child not in closed_list:
+                        open_list.put(child)
                 closed_list.add(self.current_board_tuple)
 
             # self.print_all_values()

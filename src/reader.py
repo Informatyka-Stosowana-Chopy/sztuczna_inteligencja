e84ab40bd40c9e4d4996544d494b338780417603
Przemyslaw Lis <229940@edu.p.lodz.pl>
2021-11-07 13:24:22 +0100

    cleanup

--- src/reader.py
+++ src/reader.py
@@ -5,7 +5,6 @@ class Reader:
 
     @staticmethod
     def read():
-        a = []
         with open(os.path.join(os.getcwd(), "data/board.txt"), 'r') as file:
             result = [[int(x) for x in line.split()] for line in file]
         return result

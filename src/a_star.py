53fa7df8afdc9488df5bd2adc3f62dd74a92694e
Przemyslaw Lis <229940@edu.p.lodz.pl>
2022-02-03 21:44:15 +0100

    add exception

--- src/a_star.py
+++ src/a_star.py
@@ -24,31 +24,35 @@ class Astar(Algorithm):
 
         open_list.put(self.node)
         while not open_list.empty():
-            if self.is_solved():
-                return self._print_and_return_results()
+            if self.move_counter < 1000:
+                if self.is_solved():
+                    return self._print_and_return_results()
 
-            self.move_counter += 1
-            self.node = open_list.get()
-            self.amount_of_visited_nodes += 1
+                self.move_counter += 1
+                self.node = open_list.get()
+                self.amount_of_visited_nodes += 1
 
-            if len(self.node.way) > self.reached_max_depth:
-                self.reached_max_depth = len(self.node.way)
+                if len(self.node.way) > self.reached_max_depth:
+                    self.reached_max_depth = len(self.node.way)
 
-            if self.node.current_board_tuple not in closed_list:
-                self.node.get_children()
+                if self.node.current_board_tuple not in closed_list:
+                    self.node.get_children()
 
-                for child in self.node.children:
-                    child.distance = self.calculate_heuristic(child.current_board)
-                    self.node.children_distance[child] = child.distance
+                    for child in self.node.children:
+                        child.distance = self.calculate_heuristic(child.current_board)
+                        self.node.children_distance[child] = child.distance
 
-                minimum_distance = min(self.node.children_distance.values())
-                nodes_with_minimum_distance = [node for node in self.node.children_distance if
-                                               self.node.children_distance[node] == minimum_distance]
+                    minimum_distance = min(self.node.children_distance.values())
+                    nodes_with_minimum_distance = [node for node in self.node.children_distance if
+                                                   self.node.children_distance[node] == minimum_distance]
 
-                for child in nodes_with_minimum_distance:
-                    open_list.put(child)
-                self.amount_of_processed_nodes += 1
-
+                    for child in nodes_with_minimum_distance:
+                        open_list.put(child)
+                    self.amount_of_processed_nodes += 1
+            else:
+                self.len_of_solution = -1
+                return -1
+            
         self.len_of_solution = -1
         return -1
 
